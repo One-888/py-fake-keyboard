a214415395016c68f65b664715f0d6c05a6d16a9
One-888 <53633937+One-888@users.noreply.github.com>
2022-10-10 10:19:09 -0400

    Add files via upload

---
+++ fake_key.py
@@ -0,0 +1,9 @@
+import time
+import keyboard
+
+print('start fake key')
+while True:
+    print('start fake key  ..')
+    print('Press Ctrl+C to Stop')
+    keyboard.press('esc')
+    time.sleep(60)
